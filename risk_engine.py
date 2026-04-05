bb245147bf10cdd2ef64d0ffb4ce7392996661ad
Abdulla A. B. <babulwriting@gmail.com>
2026-04-05 16:54:05 +0600

    Update risk_engine.py

--- risk_engine.py
+++ risk_engine.py
@@ -1,1 +1,35 @@
+def back_pain_risk(age,bmi,work_type):
 
+    score = 0
+
+    if age > 40:
+        score += 15
+
+    if bmi >= 25 and bmi < 30:
+        score += 15
+
+    if bmi >= 30:
+        score += 25
+
+    if work_type == "sitting":
+        score += 15
+
+    if work_type == "lifting":
+        score += 20
+
+    return score
+
+
+def risk_level(score):
+
+    if score <=25:
+        return "LOW"
+
+    elif score <=50:
+        return "MODERATE"
+
+    elif score <=75:
+        return "HIGH"
+
+    else:
+        return "SEVERE"
